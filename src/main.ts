e92a10c5b95c24f555a83c91c579e31ab14f7d72
Leah Lundqvist <leah@pigeon.sh>
2025-03-10 19:17:58 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { Clerk } from '@clerk/clerk-js'
+import './style.css'
+
+const clerk = new Clerk("pk_test_YWRhcHRlZC1mb2FsLTQyLmNsZXJrLmFjY291bnRzLmRldiQ")
+await clerk.load()
+
+if (!clerk.user || !clerk.session) {
+  window.location.href = "/login"
+} else {
+  const token = await clerk.session.getToken()
+
+  async function fetchData() {
+    const button = document.getElementById("fetch-data-button")! as HTMLButtonElement
+    button.disabled = true
+    button.textContent = "Fetching..."
+    const response = await fetch("/hello", {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    const el = document.createElement("p")
+    el.textContent = await response.text()
+    document.getElementById("app")!.appendChild(el)
+    button.disabled = false
+    button.textContent = "Fetch Data"
+  }
+
+  const app = document.getElementById("app")! as HTMLDivElement
+  const hello = document.createElement("h1")
+  hello.textContent = `Hello ${clerk.user.fullName}`
+  app.appendChild(hello)
+
+  const button = document.createElement("button")
+  button.textContent = "Fetch Data"
+  button.onclick = fetchData
+  app.appendChild(button)
+}
